0f2c675483d073adc9765ada07c3dadb54346a0e
matt <matt@matts-MacBook-Pro.local>
2016-10-16 01:50:19 -0700

    added arrows

--- core/src/com/arrow/game/MyGame.java
+++ core/src/com/arrow/game/MyGame.java
@@ -16,62 +16,83 @@ import java.util.Iterator;
 
 public class MyGame extends ApplicationAdapter {
 
-	private Texture dropImage;
-	private Texture bucketImage;
+	private Texture monsterImage;
+	private Texture bowImage;
+	private Texture arrowImage;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Rectangle bucket;
+	private Rectangle bow;
 	private Vector3 touchPos = new Vector3();
-	private Array<Rectangle> raindrops;
+	private Array<Rectangle> monsters;
+	private Array<Rectangle> arrows;
 	private long lastDropTime;
 
 	@Override
 	public void create () {
-		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+		monsterImage = new Texture(Gdx.files.internal("swordman.png"));
+		bowImage = new Texture(Gdx.files.internal("bowman.png"));
+		arrowImage = new Texture(Gdx.files.internal("arrow.png"));
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 480, 800);
 
 		batch = new SpriteBatch();
 
-		bucket = new Rectangle();
-		bucket.x = 480 / 2 - 64 / 2;
-		bucket.y = 20;
-		bucket.width = 64;
-		bucket.height = 64;
+		bow = new Rectangle();
+		bow.x = 480 / 2 - 64 / 2;
+		bow.y = 20;
+		bow.width = 64;
+		bow.height = 64;
 
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
+		monsters = new Array<Rectangle>();
+		spawnMonster();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClearColor(0, 0.5f, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for(Rectangle raindrop: raindrops) {
-			batch.draw(dropImage, raindrop.x, raindrop.y);
+		batch.draw(bowImage, bow.x, bow.y);
+		for(Rectangle monster: monsters) {
+			batch.draw(monsterImage, monster.x, monster.y);
+		}
+		for(Rectangle arrow: arrows) {
+			batch.draw(arrowImage, arrow.x, arrow.y);
 		}
 		batch.end();
 
 		if(Gdx.input.isTouched()) {
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
-			bucket.x = touchPos.x - 64 / 2;
+			spawnArrow(touchPos);
 		}
 
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnMonster();
 
-		Iterator<Rectangle> iter = raindrops.iterator();
+		Iterator<Rectangle> iter = monsters.iterator();
 		while(iter.hasNext()) {
-			Rectangle raindrop = iter.next();
-			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-			if(raindrop.y + 64 < 0) iter.remove();
-			if(raindrop.overlaps(bucket)) {
+			Rectangle monster = iter.next();
+			monster.y -= 100 * Gdx.graphics.getDeltaTime();
+			if(monster.y + 64 < 0) iter.remove();
+			if(monster.overlaps(bow)) {
 				iter.remove();
+			}
+		}
+		if (arrows.size >= 1) {
+			Iterator<Rectangle> arrowIter = arrows.iterator();
+			while (arrowIter.hasNext()) {
+				Rectangle arrow = iter.next();
+				arrow.y += 400 * Gdx.graphics.getDeltaTime();
+				if (arrow.y - 40 > 800) iter.remove();
+				for (int i = 0; i < monsters.size; i++) {
+					if (arrow.overlaps(monsters.get(i))) {
+						iter.remove();
+						monsters.removeIndex(i);
+						break;
+					}
+				}
 			}
 		}
 	}
@@ -79,17 +100,26 @@ public class MyGame extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		dropImage.dispose();
-		bucketImage.dispose();
+		monsterImage.dispose();
+		bowImage.dispose();
 	}
 
-	private void spawnRaindrop() {
-		Rectangle raindrop = new Rectangle();
-		raindrop.x = MathUtils.random(0, 480 - 64);
-		raindrop.y = 800;
-		raindrop.width = 64;
-		raindrop.height = 64;
-		raindrops.add(raindrop);
+	private void spawnMonster() {
+		Rectangle monster = new Rectangle();
+		monster.x = MathUtils.random(0, 480 - 64);
+		monster.y = 800;
+		monster.width = 64;
+		monster.height = 64;
+		monsters.add(monster);
 		lastDropTime = TimeUtils.nanoTime();
+	}
+
+	private void spawnArrow(Vector3 startPos) {
+		Rectangle arrow = new Rectangle();
+		arrow.x = startPos.x;
+		arrow.y = 0;
+		arrow.width = 20;
+		arrow.height = 40;
+		arrows.add(arrow);
 	}
 }
